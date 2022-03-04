6baf2ec4216721069eb8af7cb86338ef21e0866d
Bhatt Nisarg <nmbhatt2001@gmail.com>
2022-03-04 12:04:26 +0530

    logout function add

--- app/src/main/java/com/example/project1/SettingFragment.java
+++ app/src/main/java/com/example/project1/SettingFragment.java
@@ -1,64 +1,82 @@
 package com.example.project1;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SettingFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class SettingFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ImageView profile_display;
+    TextView editemail,editphone,editpassword,disp_email,disp_phone;
+    ImageButton logout_btn;
+
 
     public SettingFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingFragment newInstance(String param1, String param2) {
-        SettingFragment fragment = new SettingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View view =  inflater.inflate(R.layout.fragment_setting, container, false);
+
+        profile_display = view.findViewById(R.id.dis_profile);
+        editemail = view.findViewById(R.id.edt_email);
+        editphone = view.findViewById(R.id.edt_phone);
+        editpassword = view.findViewById(R.id.edt_password);
+        disp_email = view.findViewById(R.id.dis_email);
+        disp_phone = view.findViewById(R.id.dis_phone);
+        logout_btn = view.findViewById(R.id.logout);
+
+        disp_phone.setText("1234567890");
+        disp_email.setText("abc@gmail.oom");
+
+        //setonclickevents
+        profile_display.setOnClickListener(this);
+        editemail.setOnClickListener(this);
+        editpassword.setOnClickListener(this);
+        editphone.setOnClickListener(this);
+        logout_btn.setOnClickListener(this);
+
+    return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.dis_profile:
+                break;
+            case R.id.edt_email:
+                break;
+            case R.id.edt_password:
+                break;
+            case R.id.edt_phone:
+                break;
+            case R.id.logout:
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+                prefs.edit().putBoolean("Islogin", false).apply();
+                Intent intent = new Intent(getContext(),MainActivity.class);
+                startActivity(intent);
+                break;
+
+        }
     }
 }
